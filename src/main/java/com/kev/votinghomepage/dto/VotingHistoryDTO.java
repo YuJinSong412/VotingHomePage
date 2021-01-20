07e91145b7f5d69ce7b73ede09d0553fe6515346
songij4 <songij4@gmail.com>
2021-01-21 08:48:07 +0900

    index 화면 버튼 색 변경

--- src/main/java/com/kev/votinghomepage/dto/VotingHistoryDTO.java
+++ src/main/java/com/kev/votinghomepage/dto/VotingHistoryDTO.java
@@ -1,5 +1,6 @@
 package com.kev.votinghomepage.dto;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,8 +10,12 @@ import lombok.Data;
 @Entity
 @Table(name = "vote_history")
 @Data
-public class VotingHistoryDTO {
-  
+public class VotingHistoryDTO implements Serializable {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
   @Id
   @Column(name = "MANAGER_SEQ")
   private int managerSeq;
